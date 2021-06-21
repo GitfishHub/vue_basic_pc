fe51c78518802c321f90a1ad0e7a0e2ec64f7b69
bdq <bdq>
2021-06-21 17:35:29 +0800

    去console

--- src/views/treeListNext.vue
+++ src/views/treeListNext.vue
@@ -22,6 +22,7 @@
 export default {
   data() {
     return {
+      copytable: undefined,
       tableData: [
         {
           id: 1,
@@ -70,9 +71,138 @@ export default {
       ],
     }
   },
+  mounted() {
+    this.copytable = JSON.parse(JSON.stringify(this.tableData))
+  },
   methods: {
     async selectChangeEvent({ records, data }) {
-      console.log(records, data)
+      this.setflagfalse(this.tableData, this.copytable)
+      let resarr = []
+      let resbasearr = []
+      records.map((item, index) => {
+        this.tree(data, item, [], resarr)
+      })
+      for (let i = resarr.length - 1; i >= 0; i--) {
+        this.delfather(resarr[i], resbasearr)
+      }
+      for (let i = 0; i < resbasearr.length; i++) {
+        this.setsizeone(this.tableData, resbasearr[i])
+      }
+    },
+    delfather(one, all) {
+      //去掉所有父级数组
+      let flag = true
+      for (let i = 0; i < all.length; i++) {
+        if (all[i].join().indexOf(one.join()) > -1) {
+          flag = false
+          break
+        } else if (one.join().indexOf(all[i].join()) > -1) {
+          flag = false
+          all.splice(i, 1, one)
+          break
+        }
+      }
+      if (flag) {
+        all.push(one)
+      }
+    },
+    setsizeone(data, arrone) {
+      let value = { num: undefined }
+      this.getrecursionarr(data, arrone, value)
+      this.setrecursionarr(data, arrone, value.num)
+    },
+    getrecursionarr(data, arrone, value, index = 0) {
+      if (index == arrone.length - 1) {
+        value.num = data[arrone[index]].size
+      } else {
+        let nextdata = data[arrone[index]].children
+        index++
+        this.getrecursionarr(nextdata, arrone, value, index)
+      }
+    },
+    setrecursionarr(data, arrone, value, index = 0) {
+      if (index != arrone.length - 1) {
+        let nextdata = data[arrone[index]].children
+        if (data[arrone[index]].writeflag) {
+          data[arrone[index]].size += value
+        } else {
+          data[arrone[index]].writeflag = true
+          data[arrone[index]].size = value
+        }
+        index++
+        this.setrecursionarr(nextdata, arrone, value, index)
+      }
+    },
+    /* setnewtree(resjson, arrone, data, index) {
+      if (index < arrone.length) {
+        index++;
+        if (resjson.length) {
+          let flag = false;
+          resjson.map((item) => {
+            if (item.id == data[arrone[index - 1]].id) {
+              flag = true;
+              if (index < arrone.length) {
+                this.setnewtree(
+                  item.children ? item.children : (item.children = []),
+                  arrone,
+                  data[arrone[index - 1]].children,
+                  index
+                );
+              }
+            }
+          });
+          if (!flag) {
+            resjson.push({
+              id: data[arrone[index - 1]].id,
+              size: data[arrone[index - 1]].size,
+            });
+            if (index < arrone.length) {
+              this.setnewtree(
+                (resjson[resjson.length - 1].children = []),
+                arrone,
+                data[arrone[index - 1]].children,
+                index
+              );
+            }
+          }
+        } else {
+          resjson.push({
+            id: data[arrone[index - 1]].id,
+            size: data[arrone[index - 1]].size,
+          });
+          if (index < arrone.length) {
+            this.setnewtree(
+              (resjson[resjson.length - 1].children = []),
+              arrone,
+              data[arrone[index - 1]].children,
+              index
+            );
+          }
+        }
+      }
+    }, */
+    tree(data, row, arr, resarr) {
+      for (let i = 0; i < data.length; i++) {
+        let arr2 = [...arr]
+        arr2.push(i)
+        if (data[i].id == row.id) {
+          resarr.push(arr2)
+          break
+        } else {
+          if (data[i].children) {
+            this.tree(data[i].children, row, arr2, resarr)
+          }
+        }
+      }
+    },
+    setflagfalse(data, copydata) {
+      data.map((item, index) => {
+        item.writeflag = false
+        item.size = copydata[index].size
+        if (item.children) {
+          this.setflagfalse(item.children, copydata[index].children)
+        }
+      })
     },
   },
 }
